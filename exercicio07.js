1beb2df4e36ae7d6346443cc1dd602295912111f
Is4bellyOLiveira <isabellyoliveira18072008@gmail.com>
2026-06-16 15:55:07 -0300

    Add files via upload

---
+++ exercicio07.js
@@ -0,0 +1,7 @@
+let salario=Number(prompt("escreva o seu salario atual: "));
+let aumento=salario*0.20;
+const salario1=salario+aumento
+console.log(`${salario1}`)
+alert (`o salario com o aumento vai ser ${salario1}`)
+
+
